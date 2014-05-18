dbe7eae2be17e4b078e8401be9be77e276589333
andyjefferson <andy@datanucleus.org>
2014-05-18 14:59:47 +0100

    [NUCJAVAEIGHT-12] Support OffsetTime, OffsetDateTime

---
+++ src/main/java/org/datanucleus/store/types/java8/converters/OffsetTimeLongConverter.java
@@ -0,0 +1,45 @@
+/**********************************************************************
+Copyright (c) 2014 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+**********************************************************************/
+package org.datanucleus.store.types.java8.converters;
+
+import java.time.LocalTime;
+import java.time.OffsetTime;
+
+import org.datanucleus.store.types.converters.TypeConverter;
+
+/**
+ * Class to handle the conversion between java.time.OffsetTime and a long form (nanos of day).
+ */
+public class OffsetTimeLongConverter implements TypeConverter<OffsetTime, Long>
+{
+    public OffsetTime toMemberType(Long val)
+    {
+        if (val == null)
+        {
+            return null;
+        }
+
+        LocalTime time = LocalTime.ofNanoOfDay(val);
+        return OffsetTime.from(time);
+    }
+
+    public Long toDatastoreType(OffsetTime time)
+    {
+        return time != null ? time.toLocalTime().toNanoOfDay() : null;
+    }
+}
---
+++ src/main/java/org/datanucleus/store/types/java8/converters/OffsetTimeStringConverter.java
@@ -0,0 +1,54 @@
+/**********************************************************************
+Copyright (c) 2014 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+**********************************************************************/
+package org.datanucleus.store.types.java8.converters;
+
+import java.time.OffsetTime;
+
+import org.datanucleus.store.types.converters.ColumnLengthDefiningTypeConverter;
+import org.datanucleus.store.types.converters.TypeConverter;
+
+/**
+ * Class to handle the conversion between java.time.OffsetTime and a String form.
+ */
+public class OffsetTimeStringConverter implements TypeConverter<OffsetTime, String>, ColumnLengthDefiningTypeConverter
+{
+    public OffsetTime toMemberType(String str)
+    {
+        if (str == null)
+        {
+            return null;
+        }
+
+        return OffsetTime.parse(str);
+    }
+
+    public String toDatastoreType(OffsetTime time)
+    {
+        return time != null ? time.toString() : null;
+    }
+
+    public int getDefaultColumnLength(int columnPosition)
+    {
+        if (columnPosition != 0)
+        {
+            return -1;
+        }
+        // Persist as "hh:mm:ss.SSS" when stored as string
+        return 12;
+    }
+}
